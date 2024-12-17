08cc8894c8d716ce49052a58344c88a70a64342d
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-12-17 10:25:53 +0100

    added client data

--- data.py
+++ data.py
@@ -1,7 +1,15 @@
 import os
 from dotenv import load_dotenv
 
+from espo_api_client import EspoAPI
+
 load_dotenv()
 
 NEW_CRM_API_KEY = os.getenv('NEW_CRM_API_KEY')
 OLD_CRM_API_KEY = os.getenv('OLD_CRM_API_KEY')
+
+NEW_URL = "https://www.crm.alis-is.com"
+OLD_URL = "https://www.alis-is.com"
+
+NEW_CLIENT = EspoAPI(NEW_URL, NEW_CRM_API_KEY)
+OLD_CLIENT = EspoAPI(OLD_URL, OLD_CRM_API_KEY)
